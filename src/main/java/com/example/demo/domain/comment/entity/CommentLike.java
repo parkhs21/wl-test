30a39844e987d987b7e27d1961c7c5c0468aec84
parkhs21 <2001phs92@gmail.com>
2024-06-25 13:36:21 +0900

    feat: comment like 기본 entity 생성

--- src/main/java/com/example/demo/domain/comment/entity/CommentLike.java
+++ src/main/java/com/example/demo/domain/comment/entity/CommentLike.java
@@ -1,4 +1,18 @@
 package com.example.demo.domain.comment.entity;
 
+import com.example.demo.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CommentLike {
+
+        @Id
+        @Column(name = "comment_like_id")
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
 }
