af8e7be0336462b4e64cca41dcb2995629185a1f
parkhs21 <2001phs92@gmail.com>
2024-06-25 13:12:49 +0900

    feat: user,board,comment 기본 entity 생성

---
+++ src/main/java/com/example/demo/domain/board/entity/Board.java
@@ -0,0 +1,24 @@
+package com.example.demo.domain.board.entity;
+
+import com.example.demo.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Board extends BaseEntity {
+
+    @Id
+    @Column(name = "board_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String title;
+
+    @Column(length = 1000, nullable = false)
+    private String content;
+}
---
+++ src/main/java/com/example/demo/domain/comment/entity/Comment.java
@@ -0,0 +1,21 @@
+package com.example.demo.domain.comment.entity;
+
+import com.example.demo.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseEntity {
+
+    @Id
+    @Column(name = "comment_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String content;
+}
