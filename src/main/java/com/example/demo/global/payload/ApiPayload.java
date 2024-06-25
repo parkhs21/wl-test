fca8cc5f9834c586b45dd2c866d1713bfa7312a1
parkhs21 <2001phs92@gmail.com>
2024-06-25 16:57:33 +0900

    feat: 기본 반환값(ApiPayload) 및 관련 내용 작성

---
+++ src/main/java/com/example/demo/global/payload/ApiPayload.java
@@ -0,0 +1,34 @@
+package com.example.demo.global.payload;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonPropertyOrder({"isSuccess", "code", "message", "data"})
+public class ApiPayload<T> {
+
+    @JsonProperty("isSuccess")
+    private final Boolean isSuccess;
+    private final String code;
+    private final String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T data;
+
+    // 성공
+    public static <T> ApiPayload<T> onSuccess(BaseStatus status, T data) {
+        return new ApiPayload<>(true, status.getReason().code(), status.getReason().message(), data);
+    }
+
+    // 실패
+    public static <T> ApiPayload<T> onFailure(BaseStatus status, T data) {
+        return new ApiPayload<>(false, status.getReason().code(), status.getReason().message(), data);
+    }
+
+    public static <T> ApiPayload<T> onFailure(String code, String message, T data) {
+        return new ApiPayload<>(false, code, message, data);
+    }
+}
---
+++ src/main/java/com/example/demo/global/payload/BaseStatus.java
@@ -0,0 +1,6 @@
+package com.example.demo.global.payload;
+
+public interface BaseStatus {
+
+    public ReasonDTO getReason();
+}
