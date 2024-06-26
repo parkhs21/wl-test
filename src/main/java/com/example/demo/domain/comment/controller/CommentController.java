7f21c159287a1d682b67d4bd2db8659a01b56104
parkhs21 <2001phs92@gmail.com>
2024-06-26 14:24:52 +0900

    feat: comment controller에 인터페이스 구현

---
+++ src/main/java/com/example/demo/domain/comment/controller/CommentController.java
@@ -0,0 +1,55 @@
+package com.example.demo.domain.comment.controller;
+
+import com.example.demo.domain.board.dto.request.BoardCreateReq;
+import com.example.demo.domain.comment.controller.docs.CommentControllerDocs;
+import com.example.demo.domain.comment.dto.request.CommentCreateReq;
+import com.example.demo.domain.comment.dto.request.CommentUpdateReq;
+import com.example.demo.domain.comment.dto.response.CommentGetRes;
+import com.example.demo.global.payload.ApiPayload;
+import com.example.demo.global.payload.CommonSuccessStatus;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/comment")
+public class CommentController implements CommentControllerDocs {
+    @PostMapping("")
+    public ApiPayload<?> createComment(@RequestParam("user_id") Long userId,
+                                       @Valid @RequestBody CommentCreateReq req) {
+        return ApiPayload.onSuccess(CommonSuccessStatus.CREATED, null);
+    }
+
+    @GetMapping("/{comment_id}")
+    public ApiPayload<CommentGetRes> getComment(@PathVariable("comment_id") Long commentId) {
+        return ApiPayload.onSuccess(CommonSuccessStatus.OK, null);
+    }
+
+    @PutMapping("/{comment_id}")
+    public ApiPayload<?> updateComment(@PathVariable("comment_id") Long commentId,
+                                       @RequestParam("user_id") Long userId,
+                                       @Valid @RequestBody CommentUpdateReq req) {
+        return ApiPayload.onSuccess(CommonSuccessStatus.OK, null);
+    }
+
+    @DeleteMapping("/{comment_id}")
+    public ApiPayload<?> deleteComment(@PathVariable("comment_id") Long commentId,
+                                       @RequestParam("user_id") Long userId) {
+        return ApiPayload.onSuccess(CommonSuccessStatus.OK, null);
+    }
+
+    @PostMapping("/{comment_id}/like")
+    public ApiPayload<?> likeComment(@PathVariable("comment_id") Long commentId,
+                                     @RequestParam("user_id") Long userId) {
+        return ApiPayload.onSuccess(CommonSuccessStatus.OK, null);
+    }
+
+    @DeleteMapping("/{comment_id}/like")
+    public ApiPayload<?> unlikeComment(@PathVariable("comment_id") Long commentId,
+                                       @RequestParam("user_id") Long userId) {
+        return ApiPayload.onSuccess(CommonSuccessStatus.OK, null);
+    }
+}
