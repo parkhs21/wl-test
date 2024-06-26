5e4be2fcb68339ce22bf1aaa6fd8ddb2f70d0461
parkhs21 <2001phs92@gmail.com>
2024-06-26 16:52:36 +0900

    feat: 기본 서비스, 레포지토리 작성

---
+++ src/main/java/com/example/demo/domain/board/service/BoardCommandService.java
@@ -0,0 +1,4 @@
+package com.example.demo.domain.board.service;
+
+public interface BoardCommandService {
+}
---
+++ src/main/java/com/example/demo/domain/board/service/impl/BoardCommandServiceImpl.java
@@ -0,0 +1,12 @@
+package com.example.demo.domain.board.service.impl;
+
+import com.example.demo.domain.board.service.BoardCommandService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class BoardCommandServiceImpl implements BoardCommandService {
+}
