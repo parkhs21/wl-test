3610463b46b2dcbf91bec13f68f6c3b41d6810ff
parkhs21 <2001phs92@gmail.com>
2024-06-26 10:55:36 +0900

    refactor: controller에 인터페이스 반영

--- src/main/java/com/example/demo/domain/user/controller/UserController.java
+++ src/main/java/com/example/demo/domain/user/controller/UserController.java
@@ -1,37 +1,45 @@
 package com.example.demo.domain.user.controller;
 
+import com.example.demo.domain.user.controller.docs.UserControllerDocs;
+import com.example.demo.domain.user.dto.request.UserCreateReq;
+import com.example.demo.domain.user.dto.request.UserUpdateReq;
+import com.example.demo.domain.user.dto.response.UserGetRes;
 import com.example.demo.global.payload.ApiPayload;
 import com.example.demo.global.payload.CommonSuccessStatus;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/user")
-public class UserController {
+public class UserController implements UserControllerDocs {
 
     @PostMapping("")
-    public ApiPayload<?> createUser() {
-        return ApiPayload.onSuccess(CommonSuccessStatus.OK, null);
+    public ApiPayload<?> createUser(@Valid @RequestBody UserCreateReq req) {
+        return ApiPayload.onSuccess(CommonSuccessStatus.CREATED, null);
     }
 
     @GetMapping("")
-    public ApiPayload<?> getUser() {
+    public ApiPayload<UserGetRes> getUser(@RequestParam("id") Long id) {
         return ApiPayload.onSuccess(CommonSuccessStatus.OK, null);
     }
 
     @PutMapping("")
-    public ApiPayload<?> updateUser() {
+    public ApiPayload<?> updateUser(@RequestParam("id") Long id,
+                                    @Valid @RequestBody UserUpdateReq req) {
         return ApiPayload.onSuccess(CommonSuccessStatus.OK, null);
     }
 
     @DeleteMapping("")
-    public ApiPayload<?> deleteUser() {
+    public ApiPayload<?> deleteUser(@RequestParam("id") Long id) {
         return ApiPayload.onSuccess(CommonSuccessStatus.OK, null);
     }
 
-    @DeleteMapping("/hard")
-    public ApiPayload<?> hardDeleteUser() {
+    @DeleteMapping("/hard-delete")
+    public ApiPayload<?> hardDeleteUser(@RequestParam("id") Long id) {
         return ApiPayload.onSuccess(CommonSuccessStatus.OK, null);
     }
 }
